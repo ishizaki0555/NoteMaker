a39a4bfee1ec063c1b30c251055e6a84e2939a08
STUDENT\2412002 <ishizaki0555@icloud.com>
2026-09-10 12:18:57 +0900

    .dlファイルを作る用のウィンドウを作成しました

---
+++ Assets/Scripts/Presenter/Save/DLFileSavePresenter.cs
@@ -0,0 +1,22 @@
+// ========================================
+//
+// NoteMaker Project
+//
+// ========================================
+//
+// DLFileSavePresenter.cs
+//
+// 作曲者やタイトルなどの情報を含む、WAV ファイルのメタデータを保存します。
+//
+//========================================
+
+using System.Windows.Forms;
+using UnityEngine;
+
+public class DLFileSavePresenter : MonoBehaviour
+{
+    [SerializeField] private Button openButton;                 // 開くボタン
+    [SerializeField] private GameObject DLWriteWindow;          // DL 書き込みウィンドウ
+
+    
+}
